99a206b3b9535ee47c71b351d7b5854f3276efe7
Lan Nguyen <lannguyen@Lans-MBP.lan>
2022-04-03 20:31:39 -0700

    adding state to characters array, delete button and delete function

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import Table from "./Table";
 
-const characters = [
+function MyApp() 
+{
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -18,18 +20,21 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
+  ]);
 
-function MyApp() {
-    return (
-        <div>
-            <h1>Hello, React!</h1>
-      
-            <div className="container">
-                <Table characterData={characters} />
-            </div>
-        </div>
-    );  
+  function removeOneCharacter(index)
+  {
+    const updated = characters.filter((character,i) => {
+      return i!== index
+    });
+    setCharacters(updated);
+  }
+
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+  )
 }
 
 export default MyApp;
